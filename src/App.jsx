283bf54a326f324e238c820ee5314bcff932bd16
uunesn <cho010830@naver.com>
2025-05-25 23:40:39 +0900

    헤더 고정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Header from './layout/Header';
 import CourseCreatePage from './pages/CourseCreatePage';
 import HomePage from './pages/HomePage.jsx';
 import MyPage from './pages/MyPage.jsx';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/course" element={<CourseCreatePage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/course" element={<CourseCreatePage />} />
+      </Routes>
+    </>
   );
 }
 
