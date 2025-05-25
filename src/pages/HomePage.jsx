b7f3488c0faea96ecb7d60ed4bfe943f60ebd07b
uunesn <cho010830@naver.com>
2025-05-25 22:52:34 +0900

    홈, 개인인

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,51 @@
+import React from 'react';
 import '../css/HomePage.css';
 
 function HomePage() {
   return (
-    <div className="home-container">
-      <h1 className="home-title">홈페이지</h1>
+    <div>
+      <div className="hero">
+        <div className="hero-overlay">
+          <h1>Barrier-Free 여행</h1>
+          <div className="searchBox">
+            <input type="text" placeholder="여행 지역을 입력해주세요" />
+          </div>
+          <button className="travelBtn">여행하기</button>
+        </div>
+      </div>
+
+      {/* 베스트 여행지 카드 */}
+      <section className="best-destinations">
+        <h2> 베스트 여행지</h2>
+        <div className="card-container">
+
+    <div className="destination-card jeju">
+      <div className="overlay">
+        <h3>JEJU<br /><span>제주도</span></h3>
+      </div>
+    </div>
+
+    <div className="destination-card seoul">
+      <div className="overlay">
+        <h3>SEOUL<br /><span>서울</span></h3>
+      </div>
+    </div>
+
+    <div className="destination-card busan">
+      <div className="overlay">
+        <h3>BUSAN<br /><span>부산</span></h3>
+      </div>
+    </div>
+
+    <div className="destination-card suwon">
+      <div className="overlay">
+        <h3>SUWON<br /><span>수원</span></h3>
+      </div>
+    </div>
+    
+  </div>
+</section>
+
     </div>
   );
 }
